558c52a76cdad2a56b599a1e9fd8e5a378051877
visualstudio <visualstudio@122-PC39>
2019-03-06 10:53:18 -0500

    lab 2

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp13
+namespace DelegatesAndEvents
+{
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate(50);
+        }
+
+    }
+}
+
+
+namespace DelegatesAndEvents
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
         }
     }
 }
